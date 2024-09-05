4dd3e27377bcbe4ceff2a5fd8be0958d5717a847
Florian Zachs <florian.zachs@tailoryoung.com>
2024-09-05 17:03:59 +0200

    push

---
+++ Form1.cs
@@ -0,0 +1,16 @@
+using System.Text.Json.Serialization;
+
+namespace SquirrelTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World 1");
+        }
+    }
+}
